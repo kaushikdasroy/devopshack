94ac494465d28d7aa89485ec08a4589405ff47cd
kaushikdasroy <kaushikdasroy@gmail.com>
2020-04-10 15:53:49 -0700

    no change

--- eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
+++ eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import java.util.Arrays;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 
 
 @SpringBootApplication
@@ -23,22 +19,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
+	@EnableEurekaClient
+	@RestController
+	public class EurekaclientApplication {
+		public static void main(String[] args) {
+			SpringApplication.run(EurekaclientApplication.class, args);
+		}
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
-
+		@RequestMapping(value = "/")
+		public String home() {
+			return "Eureka Client application";
+		}
 
 }
 
