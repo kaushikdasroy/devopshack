174d06feeb608dce611d53af61d127afa39a3131
kaushikdasroy <kaushikdasroy@gmail.com>
2020-04-10 15:59:50 -0700

    no change

--- eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
+++ eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
@@ -5,31 +5,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
-
-
 public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@EnableEurekaClient
-	@RestController
-	public class EurekaclientApplication {
-		public static void main(String[] args) {
-			SpringApplication.run(EurekaclientApplication.class, args);
-		}
-
-		@RequestMapping(value = "/")
-		public String home() {
-			return "Eureka Client application";
-		}
-
 }
 
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaclientApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaclientApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String home() {
+		return "Eureka Client application";
+	}
+}
