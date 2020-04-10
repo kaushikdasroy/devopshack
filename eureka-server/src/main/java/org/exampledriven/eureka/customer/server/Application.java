0961979802df4cfa1317752cac4d7322023d6e3f
kaushikdasroy <kaushikdasroy@gmail.com>
2020-04-10 14:32:09 -0700

    changing springboot app

--- eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
+++ eureka-server/src/main/java/org/exampledriven/eureka/customer/server/Application.java
@@ -5,13 +5,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @SpringBootApplication
 @EnableEurekaServer
 @EnableDiscoveryClient
+
+
 public class Application {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
 }
